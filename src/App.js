368811b02ce4f511cc43b6ba533983da7f189d15
mk <v.a.stekolschikov@gmail.com>
2021-08-05 17:18:23 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash.cash)
+    const customer = useSelector(state => state.customer.customer)
+
+    const addCash = () => {
+        dispatch({type: "ADD_CASH", payload: 2})
+    }
+
+    const getCahs = () => {
+        dispatch({type: "GET_CASH", payload: 2})
+    }
+
+    const addCustomer = () => {
+        dispatch({type: "ADD_CUSTOMER", payload: 2})
+    }
+
+    const getCustomer = () => {
+        dispatch({type: "GET_CUSTOMER", payload: 2})
+    }
+
+    return (
+        <div className="App">
+            <div>customer: {customer}</div>
+            <div>cash: {cash}</div>
+            <button onClick={addCash}>cash +</button>
+            <button onClick={getCahs}>cash -</button>
+            <button onClick={addCustomer}>customer +</button>
+            <button onClick={getCustomer}>customer -</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/store/cashReducer.js
@@ -0,0 +1,14 @@
+const defaultStore = {
+    cash: 1
+}
+
+export const cashReducer = (state = defaultStore, action) => {
+    switch (action.type) {
+        case "ADD_CASH":
+            return {...state, cash: state.cash + action.payload}
+        case "GET_CASH":
+            return {...state, cash: state.cash - action.payload}
+        default:
+            return state
+    }
+}
